8f522a861b50eadedc239655ad8200a229036a6d
Tyler Peterson <petersontb@familysearch.org>
2015-04-02 11:29:50 -0600

    Eliminate long messages

--- index.js
+++ index.js
@@ -2,5 +2,11 @@
 
 var program = require('commander');
 var userArgs = process.argv.splice(2);
+var message = userArgs.join(' ');
 
-console.log(userArgs.join(' '));
+if (message.length > 140) {
+  console.log('Message was too long. Can only be 140 characters. It was: ', message.length);
+  process.exit(1);
+}
+
+console.log(message);

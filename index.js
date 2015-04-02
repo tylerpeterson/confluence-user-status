80916e7daedad15713c03aa2adf3b086de943be8
Tyler Peterson <petersontb@familysearch.org>
2015-04-02 11:48:40 -0600

    Get password from keychain.

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 #!/usr/bin/env node
 
 var program = require('commander');
+var url = require('url');
+
 var userArgs = process.argv.splice(2);
 var message = userArgs.join(' ');
-var confluenceUrl = process.env.CONFLUENCE_URL
+var confluenceUrl = process.env.CONFLUENCE_URL;
+var confluenceUser = process.env.CONFLUENCE_USER;
+var keychain = require('keychain');
 
 if (message.length > 140) {
   console.log('Message was too long. Can only be 140 characters. It was: ', message.length);
@@ -14,5 +18,14 @@ if (!confluenceUrl) {
   console.log('Please set the environment variable CONFLUENCE_URL.')
   process.exit(2);
 }
+
+if (!confluenceUser) {
+  console.log('Please set the environment variable CONFLUENCE_USER.')
+  process.exit(3);
+}
+
+keychain.getPassword({account: confluenceUser, service: url.parse(confluenceUrl).host, type: 'internet'}, function (err, pass) {
+  console.log('Password is', pass);
+});
 
 console.log(message);
